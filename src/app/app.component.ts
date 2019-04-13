069977470584b644c8f00b9930246b983042139d
github username <github email address>
2019-04-13 12:02:59 -0400

    feat(nav): add navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'natalie-zach-wedding';
+export class AppComponent implements OnInit {
+  links: Array<{label: string, value: string}>
+
+  ngOnInit() {
+    this.links = [
+      { label: 'Play', value: '' },
+      { label: 'Stay', value: '' },
+      { label: 'Eat', value: '' },
+      { label: 'Do', value: '' }
+    ];
+  }
 }
